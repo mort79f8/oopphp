7876a9716bbd7b0d83c5bf103d6e1fd61fea5117
mort79f8 <juulmorten@yahoo.com>
2020-12-20 01:27:04 +0100

    video 14

---
+++ interfaces/index.php
@@ -0,0 +1,90 @@
+<?php
+// interfaces
+interface PaymentInterface
+{
+    public function payNow();
+    public function paymentProcess();
+}
+interface LoginInterface
+{
+    public function loginFirst();
+    public function paymentProcess();
+}
+
+
+
+class Paypal implements PaymentInterface, LoginInterface
+{
+    public function loginFirst()
+    {
+    }
+
+    public function payNow()
+    {
+    }
+
+    public function paymentProcess()
+    {
+        $this->loginFirst();
+        $this->payNow();
+    }
+}
+class BankTransfer implements PaymentInterface, LoginInterface
+{
+    public function loginFirst()
+    {
+    }
+
+    public function payNow()
+    {
+    }
+
+    public function paymentProcess()
+    {
+        $this->loginFirst();
+        $this->payNow();
+    }
+}
+class Visa implements PaymentInterface
+{
+    public function payNow()
+    {
+    }
+
+    public function paymentProcess()
+    {
+        $this->payNow();
+    }
+}
+
+
+
+class Cash implements PaymentInterface
+{
+    public function payNow()
+    {
+    }
+
+    public function paymentProcess()
+    {
+        $this->payNow();
+    }
+}
+
+
+class BuyProduct
+{
+    public function pay(PaymentInterface $paymentType)
+    {
+        $paymentType->paymentProcess();
+    }
+
+    public function onlinePay(LoginInterface $paymentType)
+    {
+        $paymentType->paymentProcess();
+    }
+}
+
+$paymentType = new Cash();
+$buyProduct = new BuyProduct();
+$buyProduct->pay($paymentType);
---
+++ oopphp14/abstract/paymenttypes.abstract.php
@@ -0,0 +1,11 @@
+<?php
+
+abstract class Visa
+{
+    public function visaPayment()
+    {
+        return "Perform a payment";
+    }
+
+    abstract public function getPayment();
+}
